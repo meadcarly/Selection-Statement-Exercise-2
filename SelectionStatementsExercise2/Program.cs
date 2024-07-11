c97dd36c7ca178e8d3f354f24f8f7824a9af1914
Carly Mead <meadcarly16@gmail.com>
2024-07-11 10:00:07 -0600

    Created a questionnaire with the ability to gather user data and apply it in a switch statement

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Threading.Channels;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is so rewarding!");
+                    break;
+                case "english":
+                    Console.WriteLine("English gives you a lot of skills for the workplace.");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is an amazing teacher!");
+                    break;
+                case "history":
+                    Console.WriteLine("History can guide our future.");
+                    break;
+                default:
+                    Console.WriteLine("I love that. No education is ever wasted");
+                    break;
+            }
         }
     }
 }
